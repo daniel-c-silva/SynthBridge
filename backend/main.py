52166859efad14fa632800bb7574a231fbab0524
Daniel Silva <danielsilva@Air-de-Daniel.home>
2026-03-28 19:01:53 +0000

    updated dictionaries

--- backend/main.py
+++ backend/main.py
@@ -7,34 +7,46 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app) 
 
-# ! Dictionaries
+# ! Full Chromatic & Chord Library (Single Octave)
 notes = {
-    'C': 261.63,  'Cs': 277.18,
-    'D': 293.66,  'Ds': 311.13,
-    'E': 329.63,
-    'F': 349.23,  'Fs': 369.99,
-    'G': 392.00,  'Gs': 415.30,
-    'A': 440.00,  'As': 466.16,
-    'B': 493.88,
+    'C': 261.63, 'Cs': 277.18, 'D': 293.66, 'Ds': 311.13, 'E': 329.63, 'F': 349.23,
+    'Fs': 369.99, 'G': 392.00, 'Gs': 415.30, 'A': 440.00, 'As': 466.16, 'B': 493.88,
 }
 
 chords = {
-    # * triads
-    'C_maj': ['C', 'E', 'G'],    'C_min': ['C', 'Ds', 'G'],
-    'D_maj': ['D', 'Fs', 'A'],   'D_min': ['D', 'F', 'A'],
-    'E_maj': ['E', 'Gs', 'B'],   'E_min': ['E', 'G', 'B'],
-    'F_maj': ['F', 'A', 'C'],    'F_min': ['F', 'Gs', 'C'],
-    'G_maj': ['G', 'B', 'D'],    'G_min': ['G', 'As', 'D'],
-    'A_maj': ['A', 'Cs', 'E'],   'A_min': ['A', 'C', 'E'],
-    'B_maj': ['B', 'Ds', 'Fs'],  'B_min': ['B', 'D', 'F'],
-    
-    # * (bossa nova )
-    'C_maj7': ['C', 'E', 'G', 'B'],
-    'A_m7':   ['A', 'C', 'E', 'G'],
-    'G_7':    ['G', 'B', 'D', 'F'], # Sétima dominante
-    
-    # * diminuted
-    'B_dim': ['B', 'D', 'F'],
+    # * Major Triads (Full Chromatic Scale)
+    'C_maj': ['C', 'E', 'G'],    'Cs_maj': ['Cs', 'F', 'Gs'], 'D_maj': ['D', 'Fs', 'A'],
+    'Ds_maj': ['Ds', 'G', 'As'], 'E_maj': ['E', 'Gs', 'B'],   'F_maj': ['F', 'A', 'C'],
+    'Fs_maj': ['Fs', 'As', 'Cs'],'G_maj': ['G', 'B', 'D'],    'Gs_maj': ['Gs', 'C', 'Ds'],
+    'A_maj': ['A', 'Cs', 'E'],   'As_maj': ['As', 'D', 'F'],  'B_maj': ['B', 'Ds', 'Fs'],
+
+    # * Minor Triads (Full Chromatic Scale)
+    'C_min': ['C', 'Ds', 'G'],   'Cs_min': ['Cs', 'E', 'Gs'], 'D_min': ['D', 'F', 'A'],
+    'Ds_min': ['Ds', 'Fs', 'As'],'E_min': ['E', 'G', 'B'],    'F_min': ['F', 'Gs', 'C'],
+    'Fs_min': ['Fs', 'A', 'Cs'], 'G_min': ['G', 'As', 'D'],   'Gs_min': ['Gs', 'B', 'Ds'],
+    'A_min': ['A', 'C', 'E'],    'As_min': ['As', 'Cs', 'F'], 'B_min': ['B', 'D', 'Fs'],
+
+    # * Dominant 7ths (The tension chords)
+    'C7': ['C', 'E', 'G', 'As'],  'D7': ['D', 'Fs', 'A', 'C'], 'E7': ['E', 'Gs', 'B', 'D'],
+    'F7': ['F', 'A', 'C', 'Ds'],  'G7': ['G', 'B', 'D', 'F'],  'A7': ['A', 'Cs', 'E', 'G'],
+    'B7': ['B', 'Ds', 'Fs', 'A'],
+
+    # * Major 7ths (Bossa Nova vibe)
+    'C_maj7': ['C', 'E', 'G', 'B'],   'D_maj7': ['D', 'Fs', 'A', 'Cs'],
+    'E_maj7': ['E', 'Gs', 'B', 'Ds'], 'F_maj7': ['F', 'A', 'C', 'E'],
+    'G_maj7': ['G', 'B', 'D', 'Fs'],  'A_maj7': ['A', 'Cs', 'E', 'Gs'],
+    'Bb_maj7': ['As', 'D', 'F', 'A'],
+
+    # * Minor 7ths
+    'C_m7': ['C', 'Ds', 'G', 'As'], 'D_m7': ['D', 'F', 'A', 'C'],
+    'E_m7': ['E', 'G', 'B', 'D'],   'F_m7': ['F', 'Gs', 'C', 'Ds'],
+    'G_m7': ['G', 'As', 'D', 'F'],  'A_m7': ['A', 'C', 'E', 'G'],
+    'B_m7': ['B', 'D', 'Fs', 'A'],
+
+    # * Diminished & Augmented
+    'C_dim': ['C', 'Ds', 'Fs'], 'B_dim': ['B', 'D', 'F'],
+    'G_aug': ['G', 'B', 'Ds'],  'C_aug': ['C', 'E', 'Gs'],
+    'B_m7b5': ['B', 'D', 'F', 'A'] # Half-diminished
 }
 
 
